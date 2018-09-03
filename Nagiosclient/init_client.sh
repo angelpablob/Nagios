eb0a3156470d8f5e951ea166079172c73a5b14a4
Pablo Bordoy <angelpablob@gmail.com>
2018-09-03 10:16:17 -0700

    Updated config Nagios client scipt

--- Nagiosclient/init_client.sh
+++ Nagiosclient/init_client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker run -it -d pbordoyedrans/nagiosclient
+docker run -it -d pbordoyedrans/nagioscliente
 #Entro en loop hasta que arranque container
 while true
 do
 if [ -z "$CONT" ]; then
 	echo "Todavia no arranco container"
-	CONT=$(docker ps | grep pbordoyedrans/nagiosclient | awk  '{print $1}')
+	CONT=$(docker ps | grep pbordoyedrans/nagioscliente | awk  '{print $1}')
 else
       echo "Arranco, iniciando servicios..."
       docker exec -ti $CONT "/tmp/inicio_servicios.sh"
